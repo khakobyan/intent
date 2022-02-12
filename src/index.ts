0c6991ce5fa1345cc8cafd7e7b6d649a5ff69607
Karen Hakobyan <karenhakobyan1994@gmail.com>
2022-02-13 02:40:51 +0400

    fixed ios issue, implemented base structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native'
+import 'react-native-gesture-handler';
+import { AppRegistry, LogBox } from 'react-native'
 import { App } from './Root'
 
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent('intent', () => App)
---
+++ src/utils/end_points.ts
@@ -0,0 +1,2 @@
+export const SEARCH_BY_COUNTRY = (country: string) => `/search?country=${country}`;
+export const SEARCH_BY_NAME = (country: string, name: string) => `/search?country=${country}&name=${name}`;
